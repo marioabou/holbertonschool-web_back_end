23d949453209fe5145363d97d6683eccfe9c2f71
marioabou <mabouchotm@gmail.com>
2024-09-16 16:04:29 +0000

    Good fix

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+"""
+This module have a utility function that list all document
+"""
+import pymongo
 
 
 def list_all(mongo_collection):
-    """ List all documents in Python """
-    documents = mongo_collection.find()
-
-    if documents.count() == 0:
+    """
+    list all collections
+    """
+    if not mongo_collection:
         return []
-
-    return documents
+    return list(mongo_collection.find())
